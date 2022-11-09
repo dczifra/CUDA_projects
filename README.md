2bd0f6066651850e755d03aeef5df4b976f401be
Domonkos Czifra <doma945@gmail.com>
2022-11-09 14:10:41 +0100

    Documentation

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # CUDA_projects
-Source code from https://github.com/andriidski/random-forests-c
-Build the code with ./build.sh
-Run the code: ./main
+* Random forest source code from https://github.com/andriidski/random-forests-c
+
+# Build
+```
+./bin/build.sh
+```
+
+# Run
+C part:
+```
+./bin/main
+```
+CUDA (parallel) part
+```
+./bin/cuda
+```
+
+# Documentation
+The *main.c* source file contains a simple Random Forest implementation, where with the given
+parameters 85% accuracy can be achieved.
+
+In the *main.cu* and *parallel/train.cu* source files this implementation is modified, such that
+each Random Forest tree is computed on a different thread.
+In this way, the same accuracy can be achieved by #thread times faster.
--- parallel/train.cu
+++ parallel/train.cu
@@ -4,7 +4,6 @@ extern "C"{
 #include "../external/forest.h"
 
 struct ThreadArgs{
-    
     unsigned int thread_id;
     unsigned int num_threads;
     unsigned int n_estimators;
@@ -16,12 +15,6 @@ struct ThreadArgs{
     const RandomForestParameters *params;
     const DecisionTreeNode **random_forest;
     long int nodeId;
-};
-
-struct Args{
-    int* arr;
-    ThreadArgs* args;
-    int a,b;
 };
 
 void init_ThreadArgs(
@@ -41,23 +34,6 @@ void init_ThreadArgs(
     t->thread_id = thread_id;
     t->num_threads = num_threads;
 }
-
-//Args args;
-//int arr[10];
-//arr[0]=1;
-//arr[1]=10;
-//args.a = 11;
-//args.b = 12;
-//args.arr = &arr[0];
-//args.args = &(thread_args[i]);
-//pthread_create(&(thread_ids[i]), NULL, thread_func2, &args);
-void* thread_func2(void* _args){
-    struct Args* t = (struct Args*) _args;
-    printf("%d %d %d %d\n", t->a, t->b, t->arr[0], t->arr[1]);
-    printf("%d\n",(unsigned int) t->args->nodeId);
-    return NULL;
-}
-
     
 void* thread_func(void* _args){
     struct ThreadArgs* t_args = (struct ThreadArgs*) _args;
@@ -92,17 +68,14 @@ const DecisionTreeNode **train_model_parallel(double **data,
         init_ThreadArgs(&thread_args[i], data, params, csv_dim, ctx, i, num_threads);
         thread_args[i].random_forest = random_forest;
         thread_args[i].n_estimators = params->n_estimators;
-        //printf("Loop: %d %d %d %d\n", i, thread_args[i].num_threads, thread_args[i].n_estimators, thread_args[i].nodeId);
         
         pthread_create(&(thread_ids[i]), NULL, thread_func, &thread_args[i]);
-        //random_forest[i] = train_model_tree(data, params, csv_dim, &nodeId, ctx);
     }
 
     // === Join threads ===
     for(size_t thread_id = 0; thread_id < num_threads; ++thread_id){
         pthread_join(thread_ids[thread_id], NULL);
     }
-    //pthread_exit(NULL);
     printf("\nThreads joined\n");
 
     return random_forest;
